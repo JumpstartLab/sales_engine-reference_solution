97745e0987fcff00cdcac758d96a8871332795ba
Justin Ko <jko170@gmail.com>
2012-03-28 17:47:03 -0600

    Customer#invoices

--- lib/sales_engine/models/customer.rb
+++ lib/sales_engine/models/customer.rb
@@ -4,6 +4,10 @@ module SalesEngine
       include SalesEngine::Models::Model
 
       CSV_FILE_NAME = 'customers.csv'
+
+      def invoices
+        Invoice.find_all_by_customer_id(id)
+      end
     end
   end
 end
--- spec/sales_engine/models/customer_spec.rb
+++ spec/sales_engine/models/customer_spec.rb
@@ -1,3 +1,13 @@
 describe SalesEngine::Models::Customer do
   include_context 'model examples'
+
+  describe '#invoices' do
+    let!(:customer) { add_instance(:customer, id: 1) }
+    let!(:matching_invoice) { add_instance(:invoice, customer_id: 1) }
+    before { add_instance(:invoice, customer_id: 2) }
+
+    it "returns the Invoice instances whose customer_id matches the customer's id" do
+      customer.invoices.should =~ [matching_invoice]
+    end
+  end
 end
